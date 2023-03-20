bcbc518716ad6dbc052776ae0436a76789ea90de
eliyahukoren <elik.koren@gmail.com>
2023-03-20 13:36:28 +0200

    code cleanup

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ const LandingPage = ({ currentUser }) => {
 
 // NextJS Server Side Rendering
 LandingPage.getInitialProps = async context => {
-  console.log('LANDING PAGE')
   const client = buildClient(context);
   const { data } = await client.get("/api/users/currentuser");
 
